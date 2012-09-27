70b0715b57d43dbc1a75aa6aca9367488145709f
Ioan Eugen Stan <ieugen@apache.org>
2012-09-27 15:27:03 +0300

    Fixed build - added spec for testing js lib version

---
+++ todo-service/src/test/javascript/specs/libs-spec.js
@@ -0,0 +1,31 @@
+/**
+ * Test for availability of JavaScript functions/libraries.
+ * We test for a certain version. People need to be aware they can break stuff when upgrading.
+ * TODO: find a way to test bootstrap.js version ?
+ */
+
+define([
+    'underscore', 'jquery', 'backbone'
+], function (_, $, Backbone) {
+
+    describe('Test the libraries', function () {
+        describe('underscore.js', function () {
+            it('must be version 1.3.3', function () {
+                expect(_.VERSION).toEqual('1.3.3')
+            })
+        })
+
+        describe('jquery', function () {
+            it('must be version 1.7.2', function () {
+                expect(jQuery.fn.jquery).toEqual('1.8.2')
+                expect(jQuery).toEqual($)
+            })
+        })
+
+        describe('backbone', function () {
+            it('must be version 0.9.2', function () {
+                expect(Backbone.VERSION).toEqual('0.9.2')
+            })
+        })
+    })
+})
